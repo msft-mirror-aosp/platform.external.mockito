f0f1e1e710b78fab4340110d0d110718d2d7424d
Jason Monk <jmonk@google.com>
2016-12-08 14:42:09 -0500

    Add updated mockito target with v2.2.9
    Test: mm
    Change-Id: Ic19e6143a0df4733eb6da740b1e01f53253103e8

--- mockito-updated/Android.mk
+++ mockito-updated/Android.mk
@@ -17,23 +17,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ###################################################################
-# Host build
-###################################################################
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    $(call all-java-files-under, src) \
-    $(call all-java-files-under, cglib-and-asm/src)
-
-LOCAL_JAVA_LIBRARIES := junit-host objenesis-host ant
-LOCAL_MODULE := mockito-host
-LOCAL_MODULE_TAGS := optional
-LOCAL_JAVA_LANGUAGE_VERSION := 1.7
-include $(BUILD_HOST_JAVA_LIBRARY)
-
-
-###################################################################
 # Target build
 ###################################################################
 
@@ -42,7 +25,7 @@ include $(BUILD_HOST_JAVA_LIBRARY)
 # everything needed to run Mockito tests.
 include $(CLEAR_VARS)
 
-# Exclude source used to dynamically create classes since target builds use 
+# Exclude source used to dynamically create classes since target builds use
 # dexmaker instead and including it causes conflicts.
 explicit_target_excludes := \
     src/org/mockito/internal/creation/AbstractMockitoMethodProxy.java \
@@ -66,8 +49,8 @@ target_src_files := \
     $(filter-out $(explicit_target_excludes), $(target_src_files))
 
 LOCAL_SRC_FILES := $(target_src_files)
-LOCAL_JAVA_LIBRARIES := junit objenesis-target
-LOCAL_MODULE := mockito-api
+LOCAL_JAVA_LIBRARIES := junit4-target objenesis-updated-target
+LOCAL_MODULE := mockito-updated-api
 LOCAL_SDK_VERSION := 16
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_JAVA_LIBRARY)
@@ -76,47 +59,26 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 # needed to run Mockito tests on the device.
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := mockito-target
-LOCAL_STATIC_JAVA_LIBRARIES := mockito-target-minus-junit4 junit
+LOCAL_MODULE := mockito-updated-target
+LOCAL_STATIC_JAVA_LIBRARIES := mockito-updated-target-minus-junit4 junit4-target
 LOCAL_SDK_VERSION := 16
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
-# A mockito target that doesn't pull in junit. This is used to work around
-# issues caused by multiple copies of junit in the classpath, usually when a test
+# A mockito target that doesn't pull in junit4-target. This is used to work around
+# issues caused by multiple copies of junit4 in the classpath, usually when a test
 # using mockito is run using android.test.runner.
 include $(CLEAR_VARS)
-LOCAL_MODULE := mockito-target-minus-junit4
-LOCAL_STATIC_JAVA_LIBRARIES := mockito-api dexmaker dexmaker-mockmaker objenesis-target
-LOCAL_JAVA_LIBRARIES := junit
+LOCAL_MODULE := mockito-updated-target-minus-junit4
+LOCAL_STATIC_JAVA_LIBRARIES := mockito-updated-api dexmaker dexmaker-mockmaker-updated objenesis-updated-target
+LOCAL_JAVA_LIBRARIES := junit4-target
 LOCAL_SDK_VERSION := 16
 LOCAL_MODULE_TAGS := optional
 LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_STATIC_JAVA_LIBRARY)
-
-
-###################################################################
-# Host build
-###################################################################
-
-# Builds the Mockito source code, but does not include any run-time
-# dependencies.
-ifeq ($(HOST_OS),linux)
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(target_src_files)
-LOCAL_JAVA_LIBRARIES := \
-    junit-hostdex \
-    objenesis-hostdex
-LOCAL_MODULE := mockito-api-hostdex
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_HOST_DALVIK_JAVA_LIBRARY)
-endif # HOST_OS == linux
-
 
 ###################################################
 # Clean up temp vars
 ###################################################
 explicit_target_excludes :=
 target_src_files :=
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
